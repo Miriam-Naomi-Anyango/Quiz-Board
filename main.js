ff45f2475323b6a4f677175b55a2c059b39e8845
Miriam Anyango <minaoanya@gmail.com>
2022-05-15 23:26:57 +0300

    Specify on the javascript for the questions that when the correct choices have been selected the points should increase by one and an aleart of total score be diaplayed at the end, also create a new section to be the landing page before questions are arrived at.

--- main.js
+++ main.js
@@ -6,4 +6,55 @@ function next(id) {
     document.getElementsByClassName('containera')[id].style.display = "block";
     document.getElementsByClassName('containera')[id-2].style.display = "none";
     document.getElementsByClassName('containera')[id].style.display = "block";
+    document.getElementsByClassName('containera')[id-3].style.display = "none";
+    document.getElementsByClassName('containera')[id].style.display = "block";
+    document.getElementsByClassName('containera')[id-4].style.display = "none";
+    document.getElementsByClassName('containera')[id].style.display = "block";
+    document.getElementsByClassName('containera')[id-5].style.display = "none";
+    document.getElementsByClassName('containera')[id].style.display = "block";
+    document.getElementsByClassName('containera')[id-6].style.display = "none";
+    document.getElementsByClassName('containera')[id].style.display = "block";
+    document.getElementsByClassName('containera')[id-7].style.display = "none";
+    document.getElementsByClassName('containera')[id].style.display = "block";
+    document.getElementsByClassName('containera')[id-8].style.display = "none";
+    document.getElementsByClassName('containera')[id].style.display = "block";
+    document.getElementsByClassName('containera')[id-9].style.display = "none";
+    document.getElementsByClassName('containera')[id].style.display = "block";
+    document.getElementsByClassName('containera')[id-10].style.display = "none";
+    document.getElementsByClassName('containera')[id].style.display = "block";
+}
+// obtaining the final results
+function result() {
+    var score = 0;
+    if (document.getElementById('correct1').checked) {
+        score++;
+    }
+    if (document.getElementById('correct2').checked) {
+        score++;
+    }
+    if (document.getElementById('correct3').checked) {
+        score++;
+    }
+    if (document.getElementById('correct4').checked) {
+        score++;
+    }
+    if (document.getElementById('correct5').checked) {
+        score++;
+    }
+    if (document.getElementById('correct6').checked) {
+        score++;
+    }
+    if (document.getElementById('correct7').checked) {
+        score++;
+    }
+    if (document.getElementById('correct8').checked) {
+        score++;
+    }
+    if (document.getElementById('correct9').checked) {
+        score++;
+    }
+    if (document.getElementById('correct10').checked) {
+        score++;
+        alert("You have scored: "+ score);
+    }
 }
