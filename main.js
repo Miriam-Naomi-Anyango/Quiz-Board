f2eca4adc2a9db0c36f4c6f4da465f2ed5cfa9a0
Miriam Anyango <minaoanya@gmail.com>
2022-05-15 21:53:48 +0300

    Make the next question clickable such thet after selecting your choice answer and clicking the next button you are taken to the next question, define the question button.

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
 // display the first block of question
-document.getElementsByClassName('containera')[0].style.display = "block"
+document.getElementsByClassName('containera')[0].style.display = "block";
+// defining the next function
+function next(id) {
+    document.getElementsByClassName('containera')[id-1].style.display = "none";
+    document.getElementsByClassName('containera')[id].style.display = "block";
+    document.getElementsByClassName('containera')[id-2].style.display = "none";
+    document.getElementsByClassName('containera')[id].style.display = "block";
+}
